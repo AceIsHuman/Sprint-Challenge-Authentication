9b71046630594576ec24fdbf4f2af891f2966d5f
AceIsHuman <menoman1995@yahoo.com>
2019-12-14 11:56:15 -0600

    create login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const Users = require('./authModel.js');
+const genToken = require('./generateToken.js');
 
 router.post('/register', requireUser, async (req, res) => {
   const user = req.body;
@@ -11,8 +12,16 @@ router.post('/register', requireUser, async (req, res) => {
   res.status(401).json({ errorMessage: "Username is taken." });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', requireUser, async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Users.findBy({ username });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    return res.status(200).json({
+      id: user.id,
+      username: user.username,
+      token: genToken(user)
+    });
+  } res.status(401).json({ message: "Invalid credentials" });
 });
 
 function requireUser(req, res, next) {
