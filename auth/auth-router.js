fcb6806a3eb86c9174aadc98bf5e5f22fde056bb
AceIsHuman <menoman1995@yahoo.com>
2019-12-14 11:28:50 -0600

    create register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('./authModel.js');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  const user = req.body;
+  if (!user.username || !user.password) return res.status(401).json({ message: "Username and password required" });
+  user.password = bcrypt.hashSync(user.password, 12);
+  const _user = await Users.insert(user);
+  const { password, ...noPassword } = _user;
+  if (_user) res.status(200).json(noPassword);
+  res.status(401).json({ errorMessage: "Username is taken." });
 });
 
 router.post('/login', (req, res) => {
